992080cce5f7ee848e218b2afe78628ae0255253
Luc Iyer <luciyer@luciyer-ltm2.internal.salesforce.com>
2020-05-11 17:09:05 -0300

    internal auth

--- index.js
+++ index.js
@@ -54,6 +54,15 @@ app
           // Session ID & Server URL
 
           req.session.salesforce.source = "internal"
+          
+          const session_object = {
+            serverUrl: req.get("server_url"),
+            sessionId: req.get("session_id"),
+            version: process.env.API_VERSION
+          }
+
+          req.session.salesforce.connection =
+            new jsforce.Connection(session_object)
 
           next()
 
