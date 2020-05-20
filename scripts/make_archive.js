5254c3f13de834bb543bc078117c2eb81aa3b0bd
Luc Iyer <luciyer@luciyer-ltm2.internal.salesforce.com>
2020-05-19 21:37:50 -0300

    clear staging

--- scripts/make_archive.js
+++
@@ -1,3 +0,0 @@
-const deploy = require("../src/deploy.js")
-
-deploy.fromRepository(null, ["CSV_Template", "Mortgage_Calculator"])
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+require("dotenv").config()
+
+const fs = require("fs")
+const path = require("path")
+
 exports.timestamp = () => {
   return new Date().toLocaleTimeString()
 }
@@ -18,5 +23,22 @@ exports.stayAwake = () => {
 }
 
 exports.clearStaging = () => {
-  
+
+  const cwd = path.join(__dirname, '..')
+  const staging_directory = cwd + process.env.STAGING_DIR
+
+  if (fs.existsSync(staging_directory)){
+    console.log("Found tmp directory. Reinitializing.")
+    fs.rmdirSync(staging_directory, { recursive: true })
+  }
+
+  fs.mkdir(staging_directory, (err) => {
+
+      if (err)
+        return console.error(err);
+
+      console.log('Staging Directory created successfully!');
+
+  })
+
 }
